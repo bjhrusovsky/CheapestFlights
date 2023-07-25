7fe8d8ff1eb18eef24884fee0f656e166fb6f601
Romanpuss <bryce_hrusovsky@yahoo.com>
2023-07-24 20:03:16 -0700

    Added password encryption.

--- Bots/mainBot.py
+++ Bots/mainBot.py
@@ -1,9 +1,15 @@
 import Utils.BotUtils as BotUtils
 import API.GoogleSheetsAPI as sheetsAPI
+import API.sendEmail as Email
+import datetime
 def runBot():
     listOfValidFlights = BotUtils.getValidFlights()
     BotUtils.storeValidFLights(listOfValidFlights)
 
 if __name__ == "__main__":
-    runBot()
-    sheetsAPI.writeToGoogleSheets()
+    file = open(r'C:\Users\16169\PycharmProjects\CheapestFlights\Resources\test.txt', 'a')
+
+    file.write(f'{datetime.datetime.now()} - The script ran\n')
+    # runBot()
+    # sheetsAPI.writeToGoogleSheets()
+    # Email.emailUser()
