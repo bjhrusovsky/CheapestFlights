b5afa1d787050a0dc244faca1b870e2ebd06a836
Romanpuss <bryce_hrusovsky@yahoo.com>
2023-08-01 18:44:52 -0700

    Added error handling. Few more comments and documentation.

--- Bots/mainBot.py
+++ Bots/mainBot.py
@@ -1,11 +1,21 @@
 import Utils.BotUtils as BotUtils
 import API.GoogleSheetsAPI as sheetsAPI
 
+
 def exportData():
+    """
+    Write our data to our google sheets, then send out the emails.
+    :return: no return value
+    """
     sheetsAPI.writeToGoogleSheets()
     sheetsAPI.sendEmails()
 
 def runBot():
+    """
+    Execute our program. Here we gather our flight data. Store the flights in db. Then utilize
+    flight data to export to our google sheets.
+    :return:
+    """
     listOfValidFlights = BotUtils.getValidFlights()
     BotUtils.storeValidFLights(listOfValidFlights)
     exportData()
--- Utils/BotUtils.py
+++ Utils/BotUtils.py
@@ -30,27 +30,27 @@ def getValidFlights():
         startEndDateGap = endDate - startDate
         while startEndDateGap.days >= travelInterval[1]:
             flightPathParam = flightpath
-            # try:
-            currentReturnDate = (startDate + timedelta(days=travelInterval[1])).strftime('%Y-%m-%d')
-            startDatestr = startDate.strftime('%Y-%m-%d')
+            try:
+                currentReturnDate = (startDate + timedelta(days=travelInterval[1])).strftime('%Y-%m-%d')
+                startDatestr = startDate.strftime('%Y-%m-%d')
 
-            validFlights = eval(
-                "run" + website[1].lower().capitalize() + "Bot(flightPathParam, startDatestr, currentReturnDate, link)")
-            startDate = (startDate + timedelta(days=1))
-            startEndDateGap = endDate - startDate
-            listOfValidFlights.append(validFlights)
-            # except Exception as e:
-            #     print("Something went wrong within getValidFlights function.\n")
-            #     print(e)
-            #     exc_type, exc_obj, exc_tb = sys.exc_info()
-            #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            #     print("\nError Type:")
-            #     print(exc_type)
-            #     print("\nFileName:")
-            #     print(fname)
-            #     print("\nLine Number:")
-            #     print(exc_tb.tb_lineno)  # Error handling
-            #     sys.exit()
+                validFlights = eval(
+                    "run" + website[1].lower().capitalize() + "Bot(flightPathParam, startDatestr, currentReturnDate, link)")
+                startDate = (startDate + timedelta(days=1))
+                startEndDateGap = endDate - startDate
+                listOfValidFlights.append(validFlights)
+            except Exception as e:
+                print("Something went wrong within getValidFlights function.\n")
+                print(e)
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print("\nError Type:")
+                print(exc_type)
+                print("\nFileName:")
+                print(fname)
+                print("\nLine Number:")
+                print(exc_tb.tb_lineno)  # Error handling
+                sys.exit()
     return listOfValidFlights
 
 def storeValidFLights(ListOfValidFlights: list):
